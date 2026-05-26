ff13d71b7fe36993175f76d277afbf4267a79c6a
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-26 23:48:44 +0200

    Fix event listener for button to change image source and alt text

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const immagine = document.querySelector('img');
 const bottone = document.querySelector('button');
 
+button.addEventListener('click', () => {
+image.src = 'img/yellow_lamp.png';
+image.alt = 'lampadina accesa';
+});

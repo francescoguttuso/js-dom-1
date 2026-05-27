5f859293c5d586273ea74116280ec5dae9ae8065
francescoguttuso <francescoguttuso@gmail.com>
2026-05-27 12:35:09 +0200

    Refactor HTML structure and enhance JavaScript functionality for lamp control

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const immagine = document.querySelector('img');
-const bottone = document.querySelector('button');
+const immagine = document.querySelector('#imgAttiva');
+const bottone = document.querySelector('#btnAccendi');
+let accesa = false;
+bottone.addEventListener('click', function () {
+    if (!accesa) {
+        immagine.src = 'img/yellow_lamp.png';
+        bottone.textContent = 'Spegni';
+        accesa = true;
+    }
+    else {
+        immagine.src = 'img/white_lamp.png';
+        bottone.textContent = 'Accendi';
+        accesa = false;
+    }
+});
+
 
